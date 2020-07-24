c5d6eb03a68d6aa8b434b41d6fc6475f4fd6cdd4
kim4pb <kim4pb@gmail.com>
2020-07-25 03:10:52 +0900

    jamandbread

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.main_page, name='main_page'),
+    path('question1/', views.question1, name='question1'),
+    path('question2/', views.question2, name='question2'),
+    path('question3/', views.question3, name='question3'),
+    path('question4/', views.question4, name='question4'),
+    path('result/', views.result, name='result'),
+]
---
+++ blog/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from .models import Post
+
+def main_page(request):
+    return render(request, 'blog/main_page.html', {})
+
+def question1(request):
+    question1 = Post.objects.get(index=1)
+    return render(request, 'blog/question1.html', {'question1':question1})
+
+def question2(request):
+    question2 = Post.objects.get(index=2)
+    return render(request, 'blog/question2.html', {'question2':question2})
+
+def question3(request):
+    question3 = Post.objects.get(index=3)
+    return render(request, 'blog/question3.html', {'question3':question3})
+
+def question4(request):
+    question4 = Post.objects.get(index=4)
+    return render(request, 'blog/question4.html', {'question4':question4})
+
+def result(request):
+    result = Post.objects.get(index=11)
+    return render(request, 'blog/result.html', {'result':result})
